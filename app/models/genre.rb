f94a84666b7fcf750d8964d9941393938d1c514b
Andrew Waller <andrewhwaller@gmail.com>
2019-04-18 21:14:47 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.artists.each do |a|
-      a.name
+      a.artist.name
     end
   end
 end
